f5edd3ac5cd9c2ef4a04e42a6ce34941e6e54d0c
Victor Lum <viclum@zoho.com>
2022-10-31 11:27:46 +0800

    Property added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace CSharpIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); // changed on 23 Oct
+            //Console.WriteLine("Hello World!"); 
+            Student student1 = new Student();
+            student1.FirstName = "John";
+            student1.DisplayName();
+        }
+    }
+
+    class Student
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+
+        }
+
+        
+
+        public void DisplayName()
+        {
+            Console.WriteLine(firstName);
         }
     }
 }
