d7b87c2a691eb749443ee51c74e98204b1ee6504
Victor Lum <viclum@zoho.com>
2022-11-09 11:09:52 +0800

    Added CashCard class

--- Program.cs
+++ Program.cs
@@ -16,6 +16,55 @@ namespace CSharpIntro
         }
     }
 
+    class CashCard
+    {
+        // -id:string 
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        //-balance:double
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        /*+CashCard()
+         * +CashCard(string,double)
+         * +TopUp(double)
+         * +Deduct(double):bool
+         */
+        public CashCard() { }
+        public CashCard(string i, double b)
+        {
+            Id = i;
+            Balance = b;
+        }
+        public void TopUp(double amount)
+        {
+            Balance += amount;
+        }
+        public bool Deduct(double amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance = Balance - amount;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override string ToString()
+        {
+            return "Id:" + Id + " Balance:" + Balance;
+        }
+    }
+
     class Student
     {
         private string firstName;
