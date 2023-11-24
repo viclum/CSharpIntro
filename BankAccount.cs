925a70776bc244c375c03c798888287129f9186b
Victor Lum <viclum@zoho.com>
2023-11-24 15:37:46 +0800

    Changed constructor

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,4 +1,4 @@
-    namespace CSharpIntro
+namespace CSharpIntro
 {
     class BankAccount
     {
@@ -9,8 +9,12 @@
         public string AccNo { get; set; }
         public string AccName { get; set; }
         public double Balance { get; set; }
+        public BankAccount() 
+        { 
+            AccNo = "";
+            AccName = "";
+        }
 
-        public BankAccount() { }
         public BankAccount(string ano, string ana, double b)
         {
             AccNo = ano;
@@ -19,11 +23,19 @@
         }
         public void Deposit(double amount)
         {
-            Balance += amount;
+            // ensure that amount is positive
+            if (amount > 0)
+            {
+                Balance += amount;    
+            }
         }
         public bool Withdraw(double amount)
         {
-            if (amount <= Balance)
+            if (amount < 0)
+            {
+                return false;
+            }
+            else if (amount <= Balance)
             {
                 Balance = Balance - amount;
                 return true;
@@ -33,9 +45,8 @@
         }
         public override string ToString()
         {
-            return "AccNo:" + AccNo + "\tAccName:" + AccName + "\tBalance:" + Balance;
+            string str = $"AccNo:{AccNo}\tAccName:{AccName}\tBalance:{Balance}";
+            return str;
         }
     }
-
-
 }
